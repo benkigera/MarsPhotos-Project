35d356babe2142e61728b594ba69e3a8dc3f86d0
Benson Kigera <benkigera@gmail.com>
2022-05-10 15:47:47 +0300

    display grid mars photos

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -6,27 +6,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
 /**
- * Build the Moshi object with Kotlin adapter factory that Retrofit will be using.
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
  */
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-
-
 /**
- * The Retrofit object with the Moshi converter.
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
  */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
-
-
 
 /**
  * A public interface that exposes the [getPhotos] method
@@ -38,9 +36,8 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("photos")
-    suspend fun getPhotos() : List<MarsPhoto>
+    suspend fun getPhotos(): List<MarsPhoto>
 }
-
 
 /**
  * A public Api object that exposes the lazy-initialized Retrofit service
@@ -48,6 +45,3 @@ interface MarsApiService {
 object MarsApi {
     val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
 }
-
-
-
